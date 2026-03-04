0ce26a5177d43a1584a410f198b1c4cb9edce770
student <kryshtapovichmaryia@gmail.com>
2026-03-04 19:44:02 +0100

    z1 zestaw2

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,14 @@ import java.awt.*;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Point point = new Point();
-        System.out.println(point.x + " " + point.y);
-        point.x = 7;
-        System.out.println(point.x + " " + point.y);
-        Point point1 = new Point();
-        System.out.println(point.toString());
-        System.out.println(point.toSvg());
-        }
+        Point p1 = new Point(5, 7);
+        System.out.println("punkt p1: " + p1);
+        Point p2 = new Point();
+        System.out.println("punkt p2: " + p2);
+        p2.setX(10);
+        p2.setY(20);
+        System.out.println("punkt p2 po zmianie: " + p2);
+        System.out.println("p2.x = " + p2.getX());
+        System.out.println("p2.y = " + p2.getY());
     }
 }
